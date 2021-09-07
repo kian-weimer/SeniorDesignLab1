b9f3ec835114c6daa8e77dcc00ada8690e4b3a7e
rhuehnergarth <56234481+rhuehnergarth@users.noreply.github.com>
2021-09-07 15:04:37 -0500

    Adding messaging service

---
+++ message_service.py
@@ -0,0 +1,14 @@
+import os
+from twilio.rest import Client
+
+
+def send_text_message(message):
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        body=message,
+        from_='+14158861418',
+        to='+15635427467'
+    )
