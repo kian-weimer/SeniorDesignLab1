b3e899ce6a927fe807cdd0d720023574e5bfbaa0
Kian Weimer <kianweimer@gmail.com>
2021-09-07 08:24:55 -0500

    venv changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/lab1/venv"
+set -gx VIRTUAL_ENV "/home/pi/SeniorDesignLab1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
