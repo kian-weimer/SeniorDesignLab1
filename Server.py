ca92d9f7761b8db69bc3010274267034bda8cd1d
Kian Weimer <kianweimer@gmail.com>
2021-09-13 17:54:54 -0500

    Enabled server activation on boot up.     The server will now start and begin transmitting temperature data     immediately after boot up. The delay is currently far too long.

--- Server.py
+++ Server.py
@@ -4,7 +4,10 @@ import pandas as pd
 from pandas.errors import EmptyDataError
 import ast
 from flask_cors import CORS
+import os
+import time
 
+TEMP_FILE = '/home/pi/SeniorDesignLab1/temps.csv'
 
 app = Flask(__name__)
 CORS(app)
@@ -12,11 +15,25 @@ api = Api(app)
 
 class Users(Resource):
     def get(self):
-        data = pd.read_csv('temps.csv')  # read local CSV
+        data = pd.read_csv(TEMP_FILE)  # read local CSV
         data = data.to_dict()  # convert dataframe to dict
         return {'data': data}, 200  # return data and 200 OK
 
 api.add_resource(Users, '/users')  # add endpoints
 
 if __name__ == '__main__':
+    print("Waiting for internet connection...")
+    response = os.system("ping -c 1 google.com")
+    timeout = 0
+
+    #and then check the response...
+    while response != 0 and timeout < 600:
+        response = os.system("ping -c 1 google.com")
+        time.sleep(1)
+        timeout = timeout + 1
+
+    time.sleep(2) # there is a small delay after servers are pingable before we can create a server
+    
+    print("Starting server...")
     app.run('0.0.0.0', 5010)  # run our Flask app
+    print("Server started!")
--- ThermometerCode.py
+++ ThermometerCode.py
@@ -3,6 +3,8 @@ from w1thermsensor import W1ThermSensor
 import pandas as pd
 from pandas.errors import EmptyDataError
 
+TEMP_FILE = '/home/pi/SeniorDesignLab1/temps.csv'
+
 def F2C(degrees):
     return (degrees - 32)*5/9
 
@@ -12,30 +14,34 @@ def C2F(degrees):
 
 def archive_temp(temp):
     import os.path
-    if os.path.isfile('temps.csv'):
+    if os.path.isfile(TEMP_FILE):
         try:
-            df = pd.read_csv('temps.csv')
-            pd.read_csv('temps.csv', usecols=['Temp(F)', 'Temp(C)'])
-            df2 = pd.DataFrame([[C2F(temp), temp]], columns=['Temp(F)', 'Temp(C)'], index=['x'])
+            df = pd.read_csv(TEMP_FILE)
+            pd.read_csv(TEMP_FILE, usecols=['Time(s)', 'Temp(F)', 'Temp(C)'])
+            df2 = pd.DataFrame([[round(time.time()), C2F(temp), temp]], columns=['Time(s)', 'Temp(F)', 'Temp(C)'], index=['x'])
             df = df.append(df2, ignore_index=True)
         except EmptyDataError as e:
-            df = pd.DataFrame([[C2F(temp), temp]], columns=['Temp(F)', 'Temp(C)'], index=['x'])
+            df = pd.DataFrame([[round(time.time()), C2F(temp), temp]], columns=['Time(s)', 'Temp(F)', 'Temp(C)'], index=['x'])
     else:
-        df = pd.DataFrame([[C2F(temp), temp]], columns=['Temp(F)', 'Temp(C)'], index=['x'])
+        df = pd.DataFrame([[round(time.time()), C2F(temp), temp]], columns=['Time(s)', 'Temp(F)', 'Temp(C)'], index=['x'])
 
-    print(len(df.index))
-    while len(df.index) > 300:
-        df = df.iloc[1:]
-        print(1)
-    print(df)
-    df.to_csv('temps.csv', mode='w', index=False, header=True)
+    df_len = len(df.index)
+    if df_len > 300:
+        df = df.iloc[(df_len - 300):]
+
+    df.to_csv(TEMP_FILE, mode='w', index=False, header=True)
 
 sensor = W1ThermSensor()
 
 while True:
+    start_time = time.time()
     temperature = sensor.get_temperature()
-    print("The temperature is %s celsius" % temperature)
     archive_temp(temperature)
-    time.sleep(1)
+    delay = time.time() - start_time
+    if delay < 1:
+        time.sleep(1 - delay) # wait for the remaining time in 1s
+    else:
+        print(delay)
+    
 
 # https://bigl.es/ds18b20-temperature-sensor-with-python-raspberry-pi/
