c058fd999b3de90a19b441f7a047a78c472b535a
Kian Weimer <kianweimer@gmail.com>
2021-09-19 21:55:25 -0500

    Fixed POST request bug.

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 from pandas.errors import EmptyDataError
@@ -11,16 +11,20 @@ from multiprocessing import Value
 
 TEMP_FILE = '/home/pi/SeniorDesignLab1/temps.csv'
 
-class Users(Resource):
+class Server(Resource):
     # STATIC VAR
     thermometer_plugged_in = None
     LCD_on = None
-
+        
     def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('LCDStatus', required=False)
+        args = parser.parse_args()  # parse arguments to dictionary
+        print(args)
         data = pd.read_csv(TEMP_FILE)  # read local CSV
         data = data.to_dict()  # convert dataframe to dict
 
-        if Users.thermometer_plugged_in.value:
+        if Server.thermometer_plugged_in.value:
             data["thermometer_plugged_in"] = "True"
             return {'data': data}, 200  # return data and 200 OK
         else:
@@ -29,7 +33,7 @@ class Users(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('LCDStatus', required=False)
+        parser.add_argument('LCDStatus', required=True)
         args = parser.parse_args()  # parse arguments to dictionary
         print(args)
         print("\n\nHERE\n\n")
@@ -37,8 +41,10 @@ class Users(Resource):
         # call the LCD toggle function here
         input = args['LCDStatus'].upper()
         if input == "\"ON\"":
+            Server.LCD_on.value = True
             print("Turn LCD ON here")
         elif input == "\"OFF\"":
+            Server.LCD_on.value = False
             print("Turn LCD OFF here")
         else:
             return {'data': {'msg': "Error 400: LCDStatus " + input + " invalid. Enter \"ON\" or \"OFF\"."}}, 400
@@ -49,11 +55,10 @@ def main(thermometer_plugged_in, LCD_on):
     app = Flask(__name__)
     CORS(app)
     api = Api(app)
-
-    Users.thermometer_plugged_in = thermometer_plugged_in
-    Users.LCD_on = LCD_on
-
-    api.add_resource(Users, '/users')  # add endpoints
+    Server.thermometer_plugged_in = thermometer_plugged_in
+    Server.LCD_on = LCD_on
+    
+    api.add_resource(Server, '/users')  # add endpoints
 
     print("Waiting for internet connection...")
     response = os.system("ping -c 1 google.com")
@@ -65,7 +70,7 @@ def main(thermometer_plugged_in, LCD_on):
         time.sleep(1)
         timeout = timeout + 1
 
-    time.sleep(5)  # there is a small delay after servers are pingable before we can create a server
+    time.sleep(2)  # there is a small delay after servers are pingable before we can create a server
     print("Starting server...")
     
     app.run('0.0.0.0', 5010)  # run our Flask app
