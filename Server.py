9fe17a0c485d71a41220f91e8ac2d6c952ca51f3
Kian Weimer <kianweimer@gmail.com>
2021-09-20 23:28:26 -0500

    Resolved major git conflict and repaired once working POST request functionality.
    Please review this commit before accepting the PR as data may have been lost.
    
    Note: There is currently an issue that occurs most times when the temp sensor is unplugged.

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 from pandas.errors import EmptyDataError
@@ -7,21 +7,33 @@ from flask_cors import CORS
 import os
 import time
 
+from multiprocessing import Value
+
 TEMP_FILE = '/home/pi/SeniorDesignLab1/temps.csv'
 
-app = Flask(__name__)
-CORS(app)
-api = Api(app)
-
-class Users(Resource):
+class Server(Resource):
+    # STATIC VAR
+    thermometer_plugged_in = None
+    LCD_on = None
+        
     def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('LCDStatus', required=False)
+        args = parser.parse_args()  # parse arguments to dictionary
+        print(args)
         data = pd.read_csv(TEMP_FILE)  # read local CSV
         data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
+
+        if Server.thermometer_plugged_in.value:
+            data["thermometer_plugged_in"] = "True"
+            return {'data': data}, 200  # return data and 200 OK
+        else:
+            data["thermometer_plugged_in"] = "False"
+            return {'data': data}, 200  # return data and 200 OK
 
     def post(self):
         parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('LCDStatus', required=False)
+        parser.add_argument('LCDStatus', required=True)
         args = parser.parse_args()  # parse arguments to dictionary
         print(args)
         print("\n\nHERE\n\n")
@@ -29,28 +41,41 @@ class Users(Resource):
         # call the LCD toggle function here
         input = args['LCDStatus'].upper()
         if input == "\"ON\"":
+            Server.LCD_on.value = True
             print("Turn LCD ON here")
         elif input == "\"OFF\"":
+            Server.LCD_on.value = False
             print("Turn LCD OFF here")
         else:
-            return {'data': {'msg': f'Error 400: LCDStatus {input} invalid. Enter `ON` or `OFF`.'}}, 400
+            return {'data': {'msg': "Error 400: LCDStatus " + input + " invalid. Enter \"ON\" or \"OFF\"."}}, 400
         return {'data': {'msg': 'Success'}}, 200  # return data with 200 OK
 
-api.add_resource(Users, '/users')  # add endpoints
 
-if __name__ == '__main__':
+def main(thermometer_plugged_in, LCD_on):
+    app = Flask(__name__)
+    CORS(app)
+    api = Api(app)
+    Server.thermometer_plugged_in = thermometer_plugged_in
+    Server.LCD_on = LCD_on
+    
+    api.add_resource(Server, '/users')  # add endpoints
+
     print("Waiting for internet connection...")
     response = os.system("ping -c 1 google.com")
     timeout = 0
 
-    #and then check the response...
+    # and then check the response...
     while response != 0 and timeout < 600:
         response = os.system("ping -c 1 google.com")
         time.sleep(1)
         timeout = timeout + 1
 
-    time.sleep(2) # there is a small delay after servers are pingable before we can create a server
-    
+    time.sleep(2)  # there is a small delay after servers are pingable before we can create a server
+
     print("Starting server...")
+    
     app.run('0.0.0.0', 5010)  # run our Flask app
-    print("Server started!")
+
+if __name__ == '__main__':
+    main(False, False)
+
