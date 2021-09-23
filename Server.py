12da81d8b875c78515b643c058224b5d46bce696
Kian Weimer <kianweimer@gmail.com>
2021-09-22 21:45:58 -0500

    the server can now send the phone number and min and max temp

--- Server.py
+++ Server.py
@@ -17,7 +17,11 @@ TEMP_FILE = '/home/pi/SeniorDesignLab1/temps.csv'
 class Server(Resource):
     # STATIC VAR
     thermometer_plugged_in = None
-    LCD_on = None
+    LCD_on   = None
+    max_temp = None
+    min_temp = None
+    phone_number = None
+    area_code = None
         
     def get(self):
         parser = reqparse.RequestParser()  # initialize
@@ -36,34 +40,65 @@ class Server(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('LCDStatus', required=True)
+        parser.add_argument('LCDStatus', required=False)
+        parser.add_argument('max_temp', required=False)
+        parser.add_argument('min_temp', required=False)
+        parser.add_argument('area_code', required=False)
+        parser.add_argument('phone_number', required=False)
         args = parser.parse_args()  # parse arguments to dictionary
         print(args)
         print("\n\nHERE\n\n")
 
         # call the LCD toggle function here
-        input = args['LCDStatus'].upper()
-        if input == "\"ON\"":
-            Server.LCD_on.value = True
-            print("Turn LCD ON here")
-            LCD.on(True)
-            LCD.website_on = True
-        elif input == "\"OFF\"":
-            Server.LCD_on.value = False
-            print("Turn LCD OFF here")
-            LCD.on(False)
-            LCD.website_on = False
+        if args['LCDStatus']:
+            input = args['LCDStatus'].upper()
+            if input == "\"ON\"":
+                Server.LCD_on.value = True
+                print("Turn LCD ON here")
+                LCD.on(True)
+                LCD.website_on = True
+                print("website: ", LCD.website_on)
+                return {'data': {'msg': 'Success, LCD turned ON'}}, 200  # return data with 200 OK
+            elif input == "\"OFF\"":
+                Server.LCD_on.value = False
+                print("Turn LCD OFF here")
+                LCD.on(False)
+                LCD.website_on = False#should be false
+                print("website: ", LCD.website_on)
+                return {'data': {'msg': 'Success, LCD turned OFF'}}, 200  # return data with 200 OK
+            else:
+                return {'data': {'msg': "Error 400: LCDStatus " + input + " invalid. Enter \"ON\" or \"OFF\"."}}, 400
+            
+        elif args['max_temp'] and args['min_temp']:
+            Server.max_temp.value = int(args['max_temp'])
+            Server.min_temp.value = int(args['max_temp'])
+            print("New max temp: ", Server.max_temp.value)
+            print("New min temp: ", Server.min_temp.value)
+            return {'data': {'msg': 'Success, Max temp: ' + str(Server.max_temp.value) + " Min temp: " + str(Server.min_temp.value)}}, 200  # return data with 200 OK
+            
+        elif args['phone_number'] and args['area_code']:
+            Server.phone_number.value = int(args['phone_number'])
+            Server.area_code.value = int(args['area_code'])
+            print("New phone number: ", Server.phone_number.value)
+            return {'data': {'msg': 'Success, New phone number: ' + str(Server.area_code.value) + str(Server.phone_number.value) + " = " + args['area_code'] + args['phone_number']}}, 200  # return data with 200 OK
+            
         else:
-            return {'data': {'msg': "Error 400: LCDStatus " + input + " invalid. Enter \"ON\" or \"OFF\"."}}, 400
-        return {'data': {'msg': 'Success'}}, 200  # return data with 200 OK
+            return {'data': {'msg': "Error 400: No valid input parameters were passed. No action taken."}}, 400
+        
+        
 
 
-def main(thermometer_plugged_in, LCD_on):
+def main(thermometer_plugged_in, LCD_on, max_temp, min_temp, phone_number, area_code):
     app = Flask(__name__)
     CORS(app)
     api = Api(app)
     Server.thermometer_plugged_in = thermometer_plugged_in
     Server.LCD_on = LCD_on
+    Server.max_temp = max_temp
+    Server.min_temp = min_temp
+    Server.phone_number = phone_number
+    Server.area_code = area_code
+
     
     api.add_resource(Server, '/users')  # add endpoints
 
--- mainController.py
+++ mainController.py
@@ -7,7 +7,8 @@ import time
 from LCD import LCD
 
 from multiprocessing import Process, Queue, Value, Manager
-from ctypes import c_bool
+from ctypes import c_bool, c_char_p
+
 from threading import Thread
 
 from Server import main
@@ -15,6 +16,7 @@ lcd = LCD()
 
 
 def button_activated(channel):
+    print("on or off: ", LCD.website_on)
     if GPIO.input(16):
         lcd.on(True)
     elif(not LCD.website_on):
@@ -22,11 +24,8 @@ def button_activated(channel):
         
 
 if __name__ == '__main__':
-    
+    print("hello")
     #should be adjustable on the website
-    min_temp = 0
-    max_temp = 31
-    phone_number = ''
     alert_sent = False
     switch_status = False
     
@@ -40,9 +39,15 @@ if __name__ == '__main__':
     # Server communication\
     thermometer_plugged_in = Value(c_bool, False)
     LCD_on = Value(c_bool, True)
-    server = Process(target=main, args=(thermometer_plugged_in, LCD_on))
+    max_temp = Value('i', True)
+    min_temp = Value('i', True)
+    max_temp.value = 31
+    min_temp.value = 0
+    phone_number = Value('i', True)
+    area_code = Value('i', True)
+    server = Process(target=main, args=(thermometer_plugged_in, LCD_on, max_temp, min_temp, phone_number, area_code))
     server.start()
-    
+        
     while True:
         start_time = time.time()
         temp = lcd.get_and_print_temp_on()
@@ -55,12 +60,17 @@ if __name__ == '__main__':
             if not thermometer_plugged_in.value:
                 thermometer_plugged_in.value = True
             
-            if(temp > max_temp and alert_sent == False):
-                print("in temp if")
-                alert_sent = True
-                message_service.send_text_message("too hot", phone_number)
-            elif(temp < max_temp):
-                alert_sent = False
+            if(temp > max_temp.value and alert_sent == "good"):
+                alert_sent = "hot"
+                message_service.send_text_message("too hot", str(phone_number.value), str(area_code.value))
+            elif(temp < max_temp.value and alert_sent = "hot"):
+                alert_sent = "good"
+                
+            if(temp < min_temp.value and alert_sent == "good"):
+                alert_sent = "cold"
+                message_service.send_text_message("cold brr", str(phone_number.value), str(area_code.value))
+            elif(temp > min_temp.value and alert_sent = "cold"):
+                alert_sent = "good"
             
         delay = time.time() - start_time
         if delay < 1:
